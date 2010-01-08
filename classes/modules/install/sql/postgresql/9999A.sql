344c8b980505b2e64c623aff37d4d87d3dd0aec2
Joel W. Reed <joelwreed@gmail.com>
2010-01-08 11:54:47 -0500

    add manager field to db

--- classes/modules/install/sql/postgresql/9999A.sql
+++ classes/modules/install/sql/postgresql/9999A.sql
@@ -1,2 +1,4 @@
 alter table users add column fte real;
 alter table users add column classified_id integer;
+alter table users add column manager_id integer;
+alter table users add constraint fk38b73479ed2a3e7a FOREIGN KEY (manager_id) REFERENCES users(id);
